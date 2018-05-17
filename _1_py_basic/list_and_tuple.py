7de35a794f24edc51f3ed2b27168b51e27326449
yellowb <122517167@qq.com>
2018-05-17 10:56:14 +0800

    add code samples for py basic

---
+++ _1_py_basic/list_and_tuple.py
@@ -0,0 +1,22 @@
+animals = ['dog', 'cat', 'pig']
+print(animals)
+print(animals[2])
+print(animals[-1])  # access by reversed index
+
+# append element
+animals.append('cow')
+print(animals)
+
+# insert element into specified position
+animals.insert(1, 'duck')
+print(animals)
+
+# remove element
+animals.pop()  # remove the tail
+print(animals)
+animals.pop(1)  # remove by specified position
+print(animals)
+
+# modify by index
+animals[1] = 'chick'
+print(animals)

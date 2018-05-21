e46bde5ad06e7772490a6cc8e606fc7cb891e341
yellowb <122517167@qq.com>
2018-05-21 13:47:01 +0800

    finish code sample for py functional programming - high order func

---
+++ _4_functional_programming/high_order_func.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# High order function sample.
+
+
+def square(x):
+    return x * x
+
+
+def high_order(x, y, func):
+    return func(x) + func(y)
+
+
+print(high_order(3, 4, square))

11cb83750e98e02531e9751292fe7a3f93a64d7e
yellowb <122517167@qq.com>
2018-05-22 13:46:01 +0800

    finish code sample for py OOP - Enum

---
+++ _7_oop_adv/enum_test.py
@@ -0,0 +1,7 @@
+# test the usage of enum
+
+from _7_oop_adv.weekday import Weekday
+
+print(Weekday.Mon)
+print(Weekday(1))
+print(Weekday.Mon == Weekday(1))
---
+++ _7_oop_adv/weekday.py
@@ -0,0 +1,15 @@
+# Sample of Enum
+
+from enum import Enum, unique
+
+
+@unique
+class Weekday(Enum):
+    Sun = 0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+

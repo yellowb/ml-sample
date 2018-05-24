383684920233a0b04d59fe107be322eb3636aef7
yellowb <122517167@qq.com>
2018-05-24 15:10:44 +0800

    finish code sample for py itertools

---
+++ _11_common_builtin_pkgs/itertools_.py
@@ -0,0 +1,22 @@
+import itertools
+
+# create an endless iterator
+loop = itertools.count(1, 2)
+for x in loop:
+    if x > 999:
+        break
+    print(x)
+
+# create an endless cycle
+cycle = itertools.cycle(['A', 'B', 'C'])
+i = 0
+for x in cycle:
+    if i > 9:
+        break
+    print(x)
+    i = i + 1
+
+# group same elements from an iterable
+groups = itertools.groupby('AAaaabBbCCcccC', key=lambda c: c.upper())
+for k, v in groups:
+    print(k, list(v))

e9632a6ba6585a2fe651f9fd8b1a9605f373119a
yellowb <122517167@qq.com>
2018-05-24 09:25:09 +0800

    finish code sample for py datetime

--- _11_common_builtin_pkgs/datetime_.py
+++ _11_common_builtin_pkgs/datetime_.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 """Samples of using datetime built-in module"""
 
@@ -33,3 +33,16 @@ dt = dt + timedelta(hours=1)
 print(dt)
 dt = dt + timedelta(minutes=-10)
 print(dt)
+
+# change timezone(default tzinfo of a datetime object is None)
+# 时区转换的关键在于，拿到一个datetime时，要获知其正确的时区，然后强制设置时区，作为基准时间
+# 利用带时区的datetime，通过astimezone()方法，可以转换到任意时区
+utc_dt = datetime.utcnow()
+print(utc_dt)
+utc_dt = utc_dt.replace(tzinfo=timezone.utc)  # you have to force set the tzinfo, but no change the time value
+print(utc_dt)
+beijing_dt = utc_dt.astimezone(timezone(timedelta(hours=8)))  # force change timezone to +8, the display also changes
+print(beijing_dt)
+tokyo_dt = utc_dt.astimezone(timezone(timedelta(hours=9)))  # force change timezone to +9
+print(tokyo_dt)
+

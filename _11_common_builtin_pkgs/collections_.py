5e38849a3b4eeeab2442707b9724a15d77982781
yellowb <122517167@qq.com>
2018-05-24 14:00:31 +0800

    finish code sample for py collections

--- _11_common_builtin_pkgs/collections_.py
+++ _11_common_builtin_pkgs/collections_.py
@@ -1,4 +1,4 @@
-from collections import namedtuple, deque, defaultdict
+from collections import namedtuple, deque, defaultdict, OrderedDict
 
 # use namedtuple to create a named tuple with named properties
 Point = namedtuple('Point', ['x', 'y'])
@@ -11,4 +11,20 @@ q = deque([1, 2, 3])
 q.append(4)
 q.popleft()
 print(q)
+
+# default dict
+dd = defaultdict(lambda: 0)
+print(dd['NotExist'])  # prints 0
+
+# ordered dict
+od = OrderedDict()
+od['a'] = 1
+od['b'] = 2
+od['c'] = 3
+for k, v in od.items():
+    print("%s -> %s" % (k, v))
+
+
+
+
 

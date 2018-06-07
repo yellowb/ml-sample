2f283c39d0d7ed4bafc324632100a17da0088237
yellowb <122517167@qq.com>
2018-06-07 14:19:25 +0800

    add code sample for py3-cookbook - data structure

---
+++ py3_cookbook/_1_data_structure/count_words.py
@@ -0,0 +1,16 @@
+""" Count the appears of each word """
+
+from collections import Counter
+
+words = [
+    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+    'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
+    'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
+    'my', 'eyes', "you're", 'under'
+]
+
+word_count = Counter(words)
+print(word_count)
+
+top_3 = word_count.most_common(3)
+print(top_3)
